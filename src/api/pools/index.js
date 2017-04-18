1cdb37fdfc2068549daa7ea837194294e0aacc6b
Yann Carlen <yann.carlen@epitech.eu>
2017-04-18 19:38:57 +0200

    post pools

--- src/api/pools/index.js
+++ src/api/pools/index.js
@@ -3,7 +3,8 @@
  */
 const Model = require('../../services/pools/pools')
 const { isInt } = require('../../../utils/validator')
-const { responseError } = require('../../../utils/msg')
+const { responseError, responseValid } = require('../../../utils/msg')
+const { isInvalid, isValidId } = require('../../../utils/validator')
 
 const get = async ctx => ctx.body = await Model.get()
 
@@ -15,4 +16,12 @@ const getMessagesId = (ctx, id) => isInt(id)
     .then(id => Model.getMessagesId(id).then(data => ctx.body = data))
     .catch(() => responseError(ctx, 400, 'id must be a number'))
 
-module.exports = { get, getId, getMessagesId }
+const post = ctx => isInvalid(ctx)
+    .then(() => isValidId(ctx.request.body.users_id, 'users')
+        .then(() => Model.post(ctx.request.body)
+            .then(() => responseValid(ctx, 'Pool Saved'))
+            .catch(() => responseError(ctx, 500, 'Invalid Pool')))
+        .catch(() => responseError(ctx, 400, 'Invalid User Id')))
+    .catch(() => responseError(ctx, ctx.invalid.body.status, ctx.invalid.body.msg))
+
+module.exports = { get, getId, getMessagesId, post }
--- src/services/pools/pools.js
+++ src/services/pools/pools.js
@@ -2,6 +2,7 @@
  * Created by carlen on 4/17/17.
  */
 const knex = require('../../../utils/db')
+const { sanitize } = require('../../../utils/msg')
 const table = 'pools'
 
 const get = () => knex.select().from(table)
@@ -10,4 +11,6 @@ const getId = id => knex(table).where('id', id)
 
 const getMessagesId = id => knex.select('*').from('messages').where('pools_id', id)
 
-module.exports = { get, getId, getMessagesId }
+const post = ({ name, users_id }) => sanitize(name).then(nameSanit => knex(table).insert({ name: nameSanit, users_id }))
+
+module.exports = { get, getId, getMessagesId, post }
